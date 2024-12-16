0dd67b985f19d8fd2bdbd303b15129bd2236b8f4
Théobinch <theo.lepilliet@edu.esiee-it.fr>
2024-12-16 15:30:35 +0100

    add menu and music

--- main.py
+++ main.py
@@ -6,25 +6,27 @@ pygame.init()
 SCREEN = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Menu")
 
-BG = pygame.image.load("assets/backkground.png")
+BG = pygame.image.load("assets/background.png")
 
+sunshine_song = pygame.mixer.Sound("assets/sunshine.ogg")
+sunshine_song.play()
 
 def get_font(size):  # Returns Press-Start-2P in the desired size
-    return pygame.font.Font("assets/font.ttf", size)
+    return pygame.font.Font("assets/norwester.otf", size)
 
 
 def play():
     while True:
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
 
-        SCREEN.fill("indigo")
+        SCREEN.fill("lemonchiffon")
 
-        PLAY_TEXT = get_font(45).render("Y'a rien pour le moment", True, "yellow")
+        PLAY_TEXT = get_font(45).render("Y'a rien pour le moment", True, "red")
         PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
         SCREEN.blit(PLAY_TEXT, PLAY_RECT)
 
         PLAY_BACK = Button(image=None, pos=(640, 460),
-                           text_input="RETOUR", font=get_font(75), base_color="White", hovering_color="yellow")
+                           text_input="RETOUR", font=get_font(75), base_color="White", hovering_color="red")
 
         PLAY_BACK.changeColor(PLAY_MOUSE_POS)
         PLAY_BACK.update(SCREEN)
@@ -35,6 +37,32 @@ def play():
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
+                    main_menu()
+
+        pygame.display.update()
+
+def difficulty():
+    while True:
+        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("lemonchiffon")
+
+        OPTIONS_TEXT = get_font(45).render("Quoicoubeh", True, "red")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
+
+        OPTIONS_BACK = Button(image=None, pos=(640, 460),
+                              text_input="RETOUR", font=get_font(75), base_color="White", hovering_color="red")
+
+        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+        OPTIONS_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
                     main_menu()
 
         pygame.display.update()
@@ -50,13 +78,15 @@ def main_menu():
         MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
 
         PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250),
-                             text_input="PLAY", font=get_font(75), base_color="white", hovering_color="White")
-        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550),
-                             text_input="QUIT", font=get_font(75), base_color="white", hovering_color="White")
+                             text_input="PLAY", font=get_font(75), base_color="lemonchiffon", hovering_color="White")
+        DIFFICULTY_BUTTON = Button(image=pygame.image.load("assets/Prout.png"), pos=(640, 400),
+                                text_input="PROUT", font=get_font(75), base_color="lemonchiffon", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit.png"), pos=(640, 550),
+                             text_input="QUIT", font=get_font(75), base_color="lemonchiffon", hovering_color="White")
 
         SCREEN.blit(MENU_TEXT, MENU_RECT)
 
-        for button in [PLAY_BUTTON, QUIT_BUTTON]:
+        for button in [PLAY_BUTTON, DIFFICULTY_BUTTON, QUIT_BUTTON]:
             button.changeColor(MENU_MOUSE_POS)
             button.update(SCREEN)
 
@@ -67,6 +97,8 @@ def main_menu():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
                     play()
+                if DIFFICULTY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    difficulty()
                 if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
                     pygame.quit()
                     sys.exit()
